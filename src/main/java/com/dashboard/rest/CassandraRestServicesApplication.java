176e5180f50ee73e81ea5404220b7622a871df98
sgupta33 <shivam.gupta7795@gmail.com>
2018-07-02 20:56:12 -0600

    Demo for Cassandra Springboot REST Service

---
+++ src/main/java/com/dashboard/rest/CassandraRestServicesApplication.java
@@ -0,0 +1,12 @@
+package com.dashboard.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraRestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraRestServicesApplication.class, args);
+	}
+}
